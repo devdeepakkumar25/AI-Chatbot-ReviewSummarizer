2bd8ff6f9f08780ad3990a6c0dd5e333be852815
devdeepakkumar25 <devdeepakkumar25@gmail.com>
2025-11-26 12:29:21 +0530

    Setup tailwind css

--- aiApp/packages/server/index.ts
+++ aiApp/packages/server/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
 app.get("/apikey", (req: Request, res: Response) => {
   res.send(process.env.OPENAI_API_KEY);
 });
